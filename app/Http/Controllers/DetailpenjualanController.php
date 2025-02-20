257683b0ae80936513f8506ff652f53bc20eedeb
EccoTyz12 <muhmmdaddanny@gmail.com>
2025-02-20 23:54:38 +0700

    git commit "Update Coyy"

--- app/Http/Controllers/DetailpenjualanController.php
+++ app/Http/Controllers/DetailpenjualanController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Detailpenjualan;
 
 
 class DetailpenjualanController extends Controller
 {
-
-
+    public function index(Request $request){
+        $keyword = $request -> keyword;
+        $detailpenjualan = detailpenjualan::where('DetailID','LIKE','%'.$keyword.'%')
+                                        ->orwhere('PenjualanID','LIKE','%'.$keyword.'%')
+                                        ->orwhere('ProdukID','LIKE','%'.$keyword.'%')
+                                        ->paginate();
+        return view('detailpenjualan.index', compact(['/detailpenjualan']));
+    }
 }
